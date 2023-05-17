2be90629c3a1ae890ff168231dc4498c7f65d746
MulyaIrwansyah15 <100134702+MulyaIrwansyah15@users.noreply.github.com>
2023-05-17 19:38:06 +0700

    Add files via upload

---
+++ Pertemuan 8/ArrayListTest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args){
+        //inisialisasi ArrayList yang hanya dapat
+        //berisi objek String
+        ArrayList <String> string = new ArrayList<String>();
+        
+        //menambah elemen
+        string.add("praktikum");
+        string.add("collection");
+        string.add("dan generics");
+        
+        //menghapus elemen
+        string.remove("praktikum");
+        System.out.println();
+        
+        //iterasi pada keseluruhan ArrayList
+        for(String s : string){
+            System.out.println(s+" ");
+        }
+    }
+}
---
+++ Pertemuan 8/BangunDatar.java
@@ -0,0 +1,10 @@
+/**
+ * File         : BangunDatar.java
+ * Penulis      : Mulya Irwansyah
+ * Deskripsi    : kelas abstrak untuk bangun datar
+ */
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+    
+}
