b296619f49e8a84f46541011415eb33c73ca1f8c
MulyaIrwansyah15 <100134702+MulyaIrwansyah15@users.noreply.github.com>
2023-06-06 11:44:54 +0700

    Add files via upload

---
+++ Pertemuan 9/PersonDAO.java
@@ -0,0 +1,9 @@
+/**
+ *  File        : PersonDAO.java
+ *  Nama        : Mulya Irwansyah / 24060121140110
+ *  Deskripsi   : interface untuk person access object
+ */
+
+public interface PersonDAO{
+    public void savePerson(Person p) throws Exception;
+ }
